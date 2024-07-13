f985db643f92dc0922e5634cf6c6b5c63463c6e3
jylkkakai <kiitsu74@gmail.com>
2024-07-13 12:07:08 +0300

    Day 1 part 1 solution.

--- 2016/day_01/c/main.c
+++ 2016/day_01/c/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 struct Coord {
@@ -24,11 +25,32 @@ int main(void) {
   // printf("%s", str);
   char *ss = NULL;
   ss = strtok(str, " ,");
+  struct Coord loc = {0, 0, 0};
 
   while (ss != NULL) {
 
-    ss = strtok(NULL, " ,");
     printf("%s\n", ss);
+    char ch = ss[0];
+    ss++;
+    int val = strtol(ss, NULL, 10);
+    printf("%c %d\n", ch, val);
+    if (ch == 'L')
+      loc.dir = (loc.dir == 0) ? 3 : loc.dir - 1;
+    else if (ch == 'R')
+      loc.dir = (loc.dir + 1) % 4;
+
+    if (loc.dir == 0)
+      loc.x += val;
+    else if (loc.dir == 1)
+      loc.y += val;
+    else if (loc.dir == 2)
+      loc.x -= val;
+    else if (loc.dir == 3)
+      loc.y -= val;
+    ss = strtok(NULL, " ,");
+    printf("%d %d %d\n", loc.dir, loc.x, loc.y);
   }
+  printf("%d %d\n", loc.x, loc.y);
+  printf("%d\n", loc.x + loc.y);
   return 0;
 }
