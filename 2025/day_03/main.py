586f982a1d99981e30d827513112eed9c28fe210
Kai Jylkkä <kiitsu74@gmail.com>
2025-12-04 16:48:20 +0200

    Day 3 part 2

--- 2025/day_03/main.py
+++ 2025/day_03/main.py
@@ -2,9 +2,11 @@
 # with open("example.txt", "r") as file:
 with open("input.txt", "r") as file:
     total1 = 0
+    total2 = 0
     for line in file: 
         line = line[:-1]
         # print(line)
+        # Part 1
         jolt = [line[0], line[1]] 
         for i in range(2, len(line)):
             if int(jolt[1]) > int(jolt[0]):
@@ -16,4 +18,19 @@ with open("input.txt", "r") as file:
 
         # print(jolt)
         total1 += int(jolt[0])*10 + int(jolt[1])
+
+        # Part 2
+        jolt = list(line[:12])
+        for i in range(12, len(line)):
+            for j in range(0, len(jolt) - 1):
+                if int(jolt[j]) < int(jolt[j + 1]):
+                    jolt[j:-1] = jolt[j + 1:]
+                    jolt[11] = line[i]
+                    break
+            if int(line[i]) > int(jolt[11]):
+                jolt[11] = line[i]
+
+        # print(int("".join(jolt)))
+        total2 += int("".join(jolt))
 print("Part 1: " + str(total1))
+print("Part 2: " + str(total2))
